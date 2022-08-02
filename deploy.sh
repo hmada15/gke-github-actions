b17ec0a2b9fd3eb0b1d276b43e435cda2fea25bf
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-08-02 23:41:46 +0200

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.0/deploy/static/provider/cloud/deploy.yaml
 
 docker build -t hmada15/multi-front:latest -t hmada15/multi-front:$GIT_SHA ./front
-docker build -t hmada15/multi-api:latest -t hmada15/multi-api:$GIT_SHA ./server
+docker build -t hmada15/multi-api:latest -t hmada15/multi-api:$GIT_SHA ./api
 docker build -t hmada15/multi-worker:latest -t hmada15/multi-worker:$GIT_SHA ./worker
 
 docker push hmada15/multi-front:latest
