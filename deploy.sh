2bb008743234cf54a3420efcb2391b78d1749989
hmada15 <31375621+hmada15@users.noreply.github.com>
2022-08-02 23:04:42 +0200

    Use GIT_SHA env to force kubernetes to pull the new images

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-docker build -t hmada15/multi-front:latest -t hmada15/multi-front ./front
-docker build -t hmada15/multi-api:latest -t hmada15/multi-api ./server
-docker build -t hmada15/multi-worker:latest -t hmada15/multi-worker ./worker
+docker build -t hmada15/multi-front:latest -t hmada15/multi-front:$GIT_SHA ./front
+docker build -t hmada15/multi-api:latest -t hmada15/multi-api:$GIT_SHA ./server
+docker build -t hmada15/multi-worker:latest -t hmada15/multi-worker:$GIT_SHA ./worker
 
 docker push hmada15/multi-front:latest
 docker push hmada15/multi-api:latest
 docker push hmada15/multi-worker:latest
 
-docker push hmada15/multi-front
-docker push hmada15/multi-api
-docker push hmada15/multi-worker
+docker push hmada15/multi-front:$GIT_SHA
+docker push hmada15/multi-api:$GIT_SHA
+docker push hmada15/multi-worker:$GIT_SHA
 
 kubectl apply -f k8s
+
+# Use $GIT_SHA to force kubernetes to pull the new images from Docker Hub
+kubectl set image deployments/api-deployment api=hmada15/multi-api:$GIT_SHA
+kubectl set image deployments/front-deployment front=hmada15/multi-front:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=hmada15/multi-worker:$GIT_SHA
+
